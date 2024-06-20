61b404e669eaba3a55818c82732149834e9dc700
Ivan <ivancruz.bht@gmail.com>
2024-06-20 15:17:15 -0600

    Bug fixes in the attention module

--- src/attention.py
+++ src/attention.py
@@ -17,12 +17,12 @@ class SelfAttention(nn.Module):
         B, T, C = x.size()
         interm_shape = (B, T, self.n_heads, self.d_head)
 
-        q,v,k = self.in_proj(x).chunk(3, dim=-1) # (B, C, T) -> (B, T, 3*C) -> 3*(B, T, C)
-        
+        q,k,v = self.in_proj(x).chunk(3, dim=-1) # (B, C, T) -> (B, T, 3*C) -> 3*(B, T, C)
+
         # (B, T, C) -> (B, T, n_heads, d_head) -> (B, n_heads, T, d_head)
         q = q.view(*interm_shape).transpose(1,2) 
-        v = v.view(*interm_shape).transpose(1,2) 
         k = k.view(*interm_shape).transpose(1,2) 
+        v = v.view(*interm_shape).transpose(1,2) 
         
         # (B, n_heads, T, d_head) @ (B, n_heads, d_head, T) -> (B, n_heads, T, T)
         attention = q @ k.transpose(-1,-2) / math.sqrt(self.d_head)
@@ -36,7 +36,7 @@ class SelfAttention(nn.Module):
         # (B, n_heads, T, T) @ (B, n_heads, T, d_head) -> (B, n_heads, T, d_head)
         output = attention @ v
         # (B, n_heads, T, d_head) -> (B, T, n_heads, d_head) -> (B, T, C)
-        output.transpose(1,2).contiguous().view(B, T, C)
+        output = output.transpose(1,2).contiguous().view(B, T, C)
         # (B, T, C) -> (B, T, C)
         output = self.out_proj(output)
 
@@ -46,8 +46,8 @@ class CrossAttention(nn.Module):
     def __init__(self, n_heads: int, embed_size: int, cross_size: int, in_proj_bias=True, out_proj_bias=True):
         super().__init__()
         self.q_proj = nn.Linear(embed_size, embed_size, bias=in_proj_bias)
-        self.k_proj = nn.Linear(cross_size, cross_size, bias=in_proj_bias)
-        self.v_proj = nn.Linear(cross_size, cross_size, bias=in_proj_bias)
+        self.k_proj = nn.Linear(cross_size, embed_size, bias=in_proj_bias)
+        self.v_proj = nn.Linear(cross_size, embed_size, bias=in_proj_bias)
         self.out_proj = nn.Linear(embed_size, embed_size, bias=out_proj_bias)
         self.n_heads = n_heads
         self.head_size = embed_size // n_heads
@@ -55,12 +55,13 @@ class CrossAttention(nn.Module):
     def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         # x: (B, T_Q, C_Q)
         # y: (B, T_KV, C_KV) = (B, 77, 768)
-        B, T, C = x.size()
-        interm_shape = (B, T, self.n_heads, self.head_size)
 
-        q = self.q_proj(x).view(*interm_shape).transpose(1,2)
-        k = self.k_proj(y).view(*interm_shape).transpose(1,2)
-        v = self.v_proj(y).view(*interm_shape).transpose(1,2)
+        B, T, C = x.size()
+        interm_shape = (B, -1, self.n_heads, self.head_size) # T=-1 because we don't know the size of the sequence (T_Q != T_KV)
+                
+        q = self.q_proj(x).view(interm_shape).transpose(1,2)
+        k = self.k_proj(y).view(interm_shape).transpose(1,2) 
+        v = self.v_proj(y).view(interm_shape).transpose(1,2)
 
         attention = q @ k.transpose(-1,-2) / math.sqrt(self.head_size)
         attention = F.softmax(attention, dim=-1)
