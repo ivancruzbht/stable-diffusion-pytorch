c38872a939d4aae099b16f14c46029b7327c95d6
Ivan <ivancruz.bht@gmail.com>
2024-06-20 15:22:51 -0600

    Bug fixes and change of layer names to match original weights

--- src/clip.py
+++ src/clip.py
@@ -11,12 +11,12 @@ class CLIPEmbedding(nn.Module):
     def __init__(self, vocab_size: int, embed_size: int, context_size: int):
         super().__init__()
         self.token_embedding = nn.Embedding(vocab_size, embed_size)
-        self.positional_embedding = nn.Parameter(torch.zeros(context_size, embed_size))
+        self.position_embedding = nn.Parameter(torch.zeros(context_size, embed_size))
 
     def forward(self, tokens: torch.LongTensor):
         # (B, T) -> (B, T, C)
         x = self.token_embedding(tokens) # (B, T) -> (B, T, C)
-        x = x + self.positional_embedding # (B, T, C) + (T, C) -> (B, T, C) via broadcasting
+        x = x + self.position_embedding # (B, T, C) + (T, C) -> (B, T, C) via broadcasting
 
         return x
 
@@ -24,27 +24,27 @@ class TransformerBlock(nn.Module):
     def __init__(self, n_head: int, embed_size: int):
         super().__init__()
 
-        self.layer_norm1 = nn.LayerNorm(embed_size)
-        self.self_attention = SelfAttention(n_head, embed_size)
-        self.layer_norm2 = nn.LayerNorm(embed_size)
-        self.linear1 = nn.Linear(embed_size, 4 * embed_size)
-        self.linear2 = nn.Linear(4 * embed_size, embed_size)
+        self.layernorm_1  = nn.LayerNorm(embed_size)
+        self.attention  = SelfAttention(n_head, embed_size)
+        self.layernorm_2  = nn.LayerNorm(embed_size)
+        self.linear_1  = nn.Linear(embed_size, 4 * embed_size)
+        self.linear_2  = nn.Linear(4 * embed_size, embed_size)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # x: (B, T, C)
         residue = x
 
         # Self-Attention
-        x = self.layer_norm1(x)
-        x = self.self_attention(x, causal_mask=True)
+        x = self.layernorm_1(x)
+        x = self.attention(x, causal_mask=True)
         x = x + residue
 
         # Feed Forward
         residue = x
-        x = self.layer_norm2(x)
-        x = self.linear1(x)
-        x = F.gelu(x)
-        x = self.linear2(x)
+        x = self.layernorm_2(x)
+        x = self.linear_1(x)
+        x = F.gelu(x) # original code uses x = x * torch.sigmoid(1.702 * x), which is QuickGELU activation function
+        x = self.linear_2(x)
         x = x + residue
 
         return x # (B, T, C)
@@ -53,11 +53,10 @@ class CLIP(nn.Module):
     def __init__(self):
         super().__init__()
         self.embedding = CLIPEmbedding(49408, 768, 77) # Vocab size, Embed size, Context size
-        self.layers = nn.Module(
-            [TransformerBlock(12, 768) for i in range(12)],
-
+        self.layers = nn.ModuleList(
+            [TransformerBlock(n_head=12, embed_size=768) for i in range(12)]
         )
-        self.layernor = nn.LayerNorm(768)
+        self.layernorm = nn.LayerNorm(768)
     
     def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
         tokens = tokens.type(torch.long)
